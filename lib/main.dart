a0f48124e7bb80d4e9bd4e450800530aa9c2eef2
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2022-07-25 01:06:21 +0530

    Add name and description

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.redAccent,
-        body: Row(
+        backgroundColor: Colors.black87,
+        body: Column(
           children: [
             CircleAvatar(
               backgroundImage: AssetImage('images/thamash.jpg'),
               radius: 80,
-              backgroundColor: Colors.yellow,
+              backgroundColor: Colors.lime,
+            ),
+            Text(
+              'ThamashW',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutter Dev',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                color: Colors.teal.shade50,
+                fontSize: 20,
+                letterSpacing: 3.5,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
